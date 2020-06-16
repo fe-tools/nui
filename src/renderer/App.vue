eceb3203b6e1d6243eed1c08633f1e9f33ba622a
Wizard67 <wizard67.hjn@gmail.com>
2020-06-16 15:53:02 +0800

    fix(image): 文件尺寸为 Bytes 时显示错误

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -98,7 +98,7 @@ export default defineComponent({
       if (bytes === 0) return 'n/a'
       const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB']
       const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1000)), 10)
-      if (i === 0) return `${bytes} ${sizes[i]})`
+      if (i === 0) return `${bytes} ${sizes[i]}`
       return `${(bytes / (1000 ** i)).toFixed(1)} ${sizes[i]}`
     }
 
